7713d0e8d4ecd28b9800a21afa7876971f810fc2
soran <soran.taak@gmail.com>
2023-08-21 10:32:21 +0330

    data types

---
+++ 04-data-types/script.js
@@ -0,0 +1,33 @@
+// String
+const firstName = 'Soran';
+
+// Number
+const age = 30;
+const temp = 95.5;
+
+// Boolean
+const haKids = true;
+
+// Null
+const aptNumber = null;
+
+// Undefined
+// let score;
+const score = undefined;
+
+// Symbol
+const id = Symbol('id');
+
+//BigIng
+const n = 900014255545855555544n;
+
+// Refrence Types
+const numbers = [1, 2, 3, 4, 5];
+
+const person = { name: 'Soran' };
+function sayHello() {
+	console.log('Hello');
+}
+
+const output = sayHello;
+console.log(output, typeof output);
