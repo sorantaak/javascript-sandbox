5ac509b60bddb8b04a969cd81c2fa84a30b6b30f
soran <soran.taak@gmail.com>
2023-08-22 09:20:57 +0330

    type coercion

---
+++ 08-type-coercion/script.js
@@ -0,0 +1,20 @@
+let x;
+x = 5 + '5';
+
+x = 5 + Number('5');
+
+x = 5 * '5';
+
+x = 5 + null;
+
+x = Number(null);
+x = Number(true);
+x = Number(false);
+
+x = 5 + true;
+
+x = 5 + false;
+
+x = 5 + undefined; // NaN
+
+console.log(x);
