e2a3da7ec6a8b9bbc24bae8f67fc598fae3cb49b
soran <soran.taak@gmail.com>
2023-08-24 17:15:23 +0330

    Date and Time

---
+++ 14-date-and-time/script.js
@@ -0,0 +1,20 @@
+let d;
+
+d = new Date();
+
+d = d.toString();
+
+d = new Date(2022, 7, 14);
+
+d = new Date('2021-07-10T12:30:00');
+d = new Date('07/10/201 12:30:00');
+d = new Date('2022-06-9');
+
+d = Date.now();
+d = new Date();
+d = d.getTime();
+d = d.valueOf();
+
+d = new Date(1666962049754);
+d = Math.floor(Date.now() / 1000);
+console.log(d);
