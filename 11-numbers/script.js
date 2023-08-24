6bd7e24a7cdf8f6054fb70e5f2aecfb593d0e130
soran <soran.taak@gmail.com>
2023-08-24 15:01:49 +0330

    working with number

---
+++ 11-numbers/script.js
@@ -0,0 +1,22 @@
+let x;
+
+const num = new Number(5);
+
+x = num.toString();
+
+x = num.toString().length;
+
+x = num.toFixed(2);
+
+x = num.toPrecision(2); //گرد کردن با توجه بوه ورودی تابع
+
+x = num.toExponential(2); //نمایی
+
+x = num.toLocaleString('fa-IR');
+
+x = num.valueOf();
+
+x = Number.MAX_VALUE;
+x = Number.MIN_VALUE;
+
+console.log(x);
