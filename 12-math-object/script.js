b8eca2d5e994f5e2aab14ccc0ab04336d0a399d7
soran <soran.taak@gmail.com>
2023-08-24 15:19:57 +0330

    The math object

---
+++ 12-math-object/script.js
@@ -0,0 +1,27 @@
+let x;
+
+x = Math.sqrt(9);
+
+x = Math.abs(-5);
+
+x = Math.round(4.6);
+x = Math.round(4.2);
+
+x = Math.ceil(4.2); // round up
+
+x = Math.floor(4.6); // round down
+
+x = Math.pow(2, 3);
+
+x = Math.min(4, 5, 6, 2);
+x = Math.max(4, 5, 6, 10);
+
+x = Math.random();
+
+x = Math.floor(Math.random() * 100 + 1);
+
+let a = 60;
+let b = 1000;
+x = Math.floor(Math.random() * (b - a) + a);
+
+console.log(x);
