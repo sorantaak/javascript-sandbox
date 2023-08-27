c80fbcd318d630ae73ca73b91c7bd4a0bd368fe7
soran <soran.taak@gmail.com>
2023-08-27 10:17:50 +0330

    declaration vs expression

---
+++ 03-function-scope/06-declaration-vs-expression/script.js
@@ -0,0 +1,12 @@
+console.log(addDollarSign(100));
+// Function Declaration
+function addDollarSign(value) {
+	return '$' + value;
+}
+
+
+console.log(addPlusSign(55));
+// Function Expression
+const addPlusSign = function (value) {
+	return '+' + value;
+};
