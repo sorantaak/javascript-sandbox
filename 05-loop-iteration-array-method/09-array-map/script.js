da80ea3fda4a97cfc96f3792ccdf9b2c845b2dfe
soran <soran.taak@gmail.com>
2023-08-28 11:52:52 +0330

    array map

---
+++ 05-loop-iteration-array-method/09-array-map/script.js
@@ -0,0 +1,68 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const doubleNumbers = numbers.map((number) => number * 2);
+console.log(doubleNumbers);
+
+// Same with forEach
+const doubleNumbers2 = [];
+numbers.forEach((number) => doubleNumbers2.push(number * 2));
+console.log(doubleNumbers2);
+
+const companies = [
+	{ name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+	{ name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+	{ name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+	{ name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+	{ name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+	{ name: 'Company Six', category: 'Finance', start: 1987, end: 2010 },
+	{ name: 'Company Seven', category: 'Auto', start: 1986, end: 1996 },
+	{ name: 'Company Eight', category: 'Technology', start: 2011, end: 2016 },
+	{ name: 'Company Nine', category: 'Retail', start: 1981, end: 1989 },
+];
+
+// Create an array of company name
+const companyName = companies.map((company) => company.name);
+console.log(companyName);
+
+// Create an array with just company and category
+const newCompanies = companies.map((company) => {
+	return {
+		name: company.name,
+		category: company.category,
+	};
+});
+console.log(newCompanies);
+
+// Create an array of the length of each company in years
+const companYear = companies.map((company) => {
+	return {
+		name: company.name,
+		length: company.end - company.start + ' years',
+	};
+});
+console.log(companYear);
+
+// Chain map method
+const squarendDouble = numbers
+	.map((number) => Math.sqrt(number))
+	.map((sqrt) => sqrt * 2);
+
+console.log(squarendDouble);
+
+const squarendDouble2 = numbers
+	.map(function (number) {
+		return Math.sqrt(number);
+	})
+	.map(function (sqrt) {
+		return sqrt * 2;
+	})
+	.map(function (sqrtDouble) {
+		return sqrtDouble * 3;
+	});
+console.log(squarendDouble2);
+
+// Chaning diffrent methods
+const evenDouble = numbers
+	.filter((number) => number % 2 === 0)
+	.map((num) => num * 2);
+console.log(evenDouble);
