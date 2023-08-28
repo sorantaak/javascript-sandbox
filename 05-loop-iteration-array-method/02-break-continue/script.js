0a3c525840a8054c101a90688a812196fc8a7297
soran <soran.taak@gmail.com>
2023-08-28 08:18:08 +0330

    breack continue

---
+++ 05-loop-iteration-array-method/02-break-continue/script.js
@@ -0,0 +1,17 @@
+// Break
+// for (let i = 0; i <= 20; i++) {
+// 	console.log(i);
+// 	if (i === 15) {
+// 		console.log('Break');
+// 		break;
+// 	}
+// }
+
+// Continue
+for (let i = 0; i <= 20; i++) {
+	if (i === 13) {
+		console.log('Skiooing 13...');
+		continue;
+	}
+	console.log(i);
+}
