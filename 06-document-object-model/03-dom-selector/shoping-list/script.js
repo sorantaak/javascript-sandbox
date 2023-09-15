7c32924d5e00e5ebdb4b5114be208bb73e5317a6
soran <soran.taak@gmail.com>
2023-09-15 11:53:55 +0330

    create dom selector folder

---
+++ 06-document-object-model/03-dom-selector/shoping-list/script.js
@@ -0,0 +1,45 @@
+let output;
+
+output = document.all;
+output = document.all[12];
+output = document.all.length;
+
+output = document.documentElement;
+
+output = document.head;
+output = document.body;
+
+output = document.head.children;
+output = document.body.children;
+
+output = document.doctype;
+output = document.domain;
+output = document.URL;
+output = document.characterSet;
+output = document.contentType;
+
+output = document.forms;
+output = document.forms[0];
+output = document.forms[0].id;
+output = document.forms[0].method;
+output = document.forms[0].action;
+
+// document.forms[0].id = 'new-id';
+
+// output = document.links;
+// output = document.links;
+// output = document.links[0];
+// output = document.links[0].href;
+// output = document.links[0].href = 'https://facebook.com';
+// output = document.links[0].id = 'google-link';
+// output = document.links[0].className = 'google-class';
+// output = document.links[0].classList;
+
+output = document.images;
+output = document.images[0];
+output = document.images[0].src;
+
+const forms = Array.from(document.forms); // convert HtmlCollection to array itrrable
+forms.forEach((form) => console.log(form));
+
+console.log(output);
